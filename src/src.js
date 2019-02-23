20c5c7a6f92fee416b08ec742573d9abf0389ea3
tomfun <tomfun1990@gmail.com>
2019-02-24 00:07:22 +0200

    fix problem with initial audio context creation

--- src/src.js
+++ src/src.js
@@ -1,69 +1,93 @@
 'use strict';
-import {DrawCircle} from './drawCircle'
-import {DrawSpectrum} from './drawSpectrum'
+import {DrawCircle} from './drawCircle';
+import {DrawSpectrum} from './drawSpectrum';
 import {DrawTime} from './drawTime';
 import {connectAudio} from './audioStreamConnect';
 import {createContext, getAudioElementSource, getMicrophoneSource} from './audioSource';
 
-const audioCtx = createContext();
+const micToggleControl = document.getElementById('mic-toggle');
 
-const circleDrawer = new DrawCircle(document.getElementById('canvasCircle'));
-const spectrumDrawer = new DrawSpectrum(document.getElementById('canvasSpectrum'));
-const timeDrawer = new DrawTime(document.getElementById('canvasTime'));
+let isAppStarted = false;
 
-const visualizeApi = connectAudio(audioCtx, {
-    cbTimeDataUnit8: timeDrawer.draw.bind(timeDrawer),
-    cbFreqDataFloat32(...args) {
-        circleDrawer.draw(...args);
-        spectrumDrawer.draw(...args);
-    },
-});
+function startApp() {
+    if (isAppStarted) {
+        return;
+    }
+    isAppStarted = true;
+
+    const audioCtx = createContext();
+
+    const circleDrawer = new DrawCircle(document.getElementById('canvasCircle'));
+    const spectrumDrawer = new DrawSpectrum(document.getElementById('canvasSpectrum'));
+    const timeDrawer = new DrawTime(document.getElementById('canvasTime'));
+
+    const visualizeApi = connectAudio(audioCtx, {
+        cbTimeDataUnit8: timeDrawer.draw.bind(timeDrawer),
+        cbFreqDataFloat32(...args) {
+            circleDrawer.draw(...args);
+            spectrumDrawer.draw(...args);
+        },
+    });
 
 // ---------------------- mute --------------------
 
-const muteToggleControl = document.getElementById('mute-toggle');
-muteToggleControl.addEventListener('click', () => {
-    const mute = !muteToggleControl.dataset['isMute'] || '';
-    muteToggleControl.value = mute ? 'unmute' : 'mute';
-    visualizeApi.outputGain(mute ? 0 : 1);
-    muteToggleControl.dataset['isMute'] = mute;
-});
+    const muteToggleControl = document.getElementById('mute-toggle');
+    muteToggleControl.addEventListener('click', () => {
+        const mute = !muteToggleControl.dataset['isMute'] || '';
+        muteToggleControl.value = mute ? 'unmute' : 'mute';
+        visualizeApi.outputGain(mute ? 0 : 1);
+        muteToggleControl.dataset['isMute'] = mute;
+    });
+
+
+    // --------------- get audio microphone ------------
+    let micSource = null;
+    micToggleControl.addEventListener('click', () => {
+        if (!micSource) {
+            getMicrophoneSource(audioCtx)
+                .then((source) => {
+                    micSource = source;
+                    visualizeApi.connect(micSource);
+                });
+
+        }
+        if (!micSource) {
+            micToggleControl.value = 'disconnect microphone';
+            micToggleControl.dataset['isMute'] = '';
+            return;
+        }
+        const mute = !micToggleControl.dataset['isMute'] || '';
+        micToggleControl.value = mute ? 'connect microphone' : 'disconnect microphone';
+        micToggleControl.dataset['isMute'] = mute;
+        if (mute) {
+            visualizeApi.disconnect(micSource);
+        } else {
+            visualizeApi.connect(micSource);
+        }
+    });
+
+    // --------------- get audio from local file ------------
+    const fileSource = getAudioElementSource(audioCtx, audio);
+    visualizeApi.connect(fileSource);
+}
+
 
 // --------------- get audio microphone ------------
 
-const micToggleControl = document.getElementById('mic-toggle');
-let micSource = null;
-micToggleControl.addEventListener('click', () => {
-    if (!micSource) {
-        getMicrophoneSource(audioCtx)
-            .then((source) => {
-                micSource = source;
-                visualizeApi.connect(micSource);
-            });
+micToggleControl.addEventListener('click', initAppOnMicClick);
 
-    }
-    if (!micSource) {
-        micToggleControl.value = 'disconnect microphone';
-        micToggleControl.dataset['isMute'] = '';
-        return;
-    }
-    const mute = !micToggleControl.dataset['isMute'] || '';
-    micToggleControl.value = mute ? 'connect microphone' : 'disconnect microphone';
-    micToggleControl.dataset['isMute'] = mute;
-    if (mute) {
-        visualizeApi.disconnect(micSource);
-    } else {
-        visualizeApi.connect(micSource);
-    }
-});
+function initAppOnMicClick() {
+    startApp(); // add another onClick listener
+    micToggleControl.removeEventListener('click', initAppOnMicClick);
+    micToggleControl.click(); // send virual click to another listener
+}
 
 // --------------- get audio from local file ------------
 
 const audio = document.getElementById('audio-source');
-const fileSource = getAudioElementSource(audioCtx, audio);
-visualizeApi.connect(fileSource);
 
 document.getElementById('file-uploader').addEventListener('change', (e) => {
+    startApp();
     const el = e.target;
     const file = el.files[0];
     audio.src = URL.createObjectURL(file);
